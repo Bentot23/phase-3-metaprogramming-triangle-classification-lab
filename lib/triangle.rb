8eae52c52f0931f7ea0a4edcdab524ba5dc90af9
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-12-04 03:38:27 -0600

    commit tree b42d8a3b37b86dced0314a0dea398ca49d0b2e37

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  
+  def initialize (a, b, c)
+    # @a = a
+    # @b = b
+    # @c = c
+    @sides = [a, b, c]
+    @sides.sort!
+  end
+
+  def kind
+    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "raises an error for triangles"
+    end
+  end
+
 end
+# binding.pry
